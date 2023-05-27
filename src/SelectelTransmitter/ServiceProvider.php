45625702bfd665dd78a68eb2d554de922a5f5ba1
cbepxbeo <cbepxbeo@mail.ru>
2023-05-27 21:36:09 +0300

    DI to provider

--- src/SelectelTransmitter/ServiceProvider.php
+++ src/SelectelTransmitter/ServiceProvider.php
@@ -4,36 +4,29 @@ namespace SelectelTransmitter;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
-/**
- * Class PasswordServiceProvider
- */
 class ServiceProvider extends BaseServiceProvider
 {
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
     public function register()
     {
         $app = $this->app;
 
         $app->singleton(Transmitter::class, function ($app) {
-            $value = env(config('selecteltransmitter.user') ?? '-');
-            return new Transmitter($value);
+            return new Transmitter(
+                config('selecteltransmitter.user'),
+                config('selecteltransmitter.password'),
+                config('selecteltransmitter.container'),
+                config('selecteltransmitter.account')
+            );
         });
-
     }
 
     public function boot()
     {
-
         $configPath = __DIR__.'/../config/selecteltransmitter.php';
         $this->publishes([
             $configPath => config_path('selecteltransmitter.php'),
         ], 'config');
-
     }
 
     public function provides()
